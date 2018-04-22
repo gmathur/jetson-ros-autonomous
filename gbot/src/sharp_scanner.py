ea82bad8947bed52fbfe4f7bc210cbc5da476c0d
Gaurav Mathur <g@gmathur.com>
2018-04-22 19:45:49 +0000

    Working sharp sensor code

--- gbot/src/sharp_scanner.py
+++ gbot/src/sharp_scanner.py
@@ -9,31 +9,23 @@ class SharpScanner:
 
     def read_adc(self, i):
         sum = 0
-        for i in range(0, 8):
-            sum += self.adc.read_adc(i, gain=GAIN)
+        for k in range(0, 8):
+            sum += self.adc.read_adc(i, gain=SharpScanner.GAIN)
 
         return sum/8
 
     def scan(self):
         # Scan straight
         min_dist = self.read_adc(0)
+        print("Straight dist", min_dist)
 
         # Scan right
 
         # Scan left
         
-        print("Distance sensor", values)
         
-        min_dist = 10000
-        if split_values[0] < min_dist:
-            min_dist = split_values[0]
-        if split_values[1] < min_dist:
-            min_dist = split_values[1]
-        if split_values[2] < min_dist:
-            min_dist = split_values[2]
-
         return min_dist
 
 if __name__ == "__main__":
-    scanner = SharpScanne()
+    scanner = SharpScanner()
     scanner.scan()
