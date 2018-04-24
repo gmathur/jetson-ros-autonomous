7bc09a2a2a3e9de7b573028d00d3dec065baa09d
Gaurav Mathur <g@gmathur.com>
2018-04-24 06:49:32 +0000

    Add timeout and fast ramping

--- gbot/src/dimension_driver.py
+++ gbot/src/dimension_driver.py
@@ -19,6 +19,12 @@ class DimensionDriver:
             self.ser.open()
         time.sleep(0.2)
 
+        # Set a 3 second timeout
+        self.send_command(14, 30)
+
+        # Fast ramping of 1/8 seconds
+        self.send_command(16, 2)
+
     def send_command(self, cmd, speed):
         values = [self.address, cmd, speed, (self.address + cmd + speed) & 127]
         msg = bytes(bytearray(values))
