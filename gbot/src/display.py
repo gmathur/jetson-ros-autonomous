b34f854ea11dbe8768e07654b1cc3b1230900598
Gaurav Mathur <g@gmathur.com>
2018-05-09 19:22:41 +0000

    Use framebuffer for playing video, showing image & text

--- gbot/src/display.py
+++ gbot/src/display.py
@@ -1,92 +1,96 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-import ST7735 as TFT
-import Adafruit_GPIO as GPIO
-import Adafruit_GPIO.SPI as SPI
+import pygame
+import os
+import sys
+import time
+from pygame.locals import *
 
+#os.environ['SDL_VIDEODRIVER'] = 'dummy' 
+os.environ["SDL_FBDEV"] = "/dev/fb1"
+os.environ['SDL_NOMOUSE'] = "1"
 
 WIDTH = 128
 HEIGHT = 128
 SPEED_HZ = 4000000
-
-# Raspberry Pi configuration.
-DC = 6
-RST = 5
-SPI_PORT = 0
-SPI_DEVICE = 0
+FPS = 24
 
 class Display:
     def __init__(self, font, assets_dir):
         rospy.loginfo("Loading font %s assets dir %s", font, assets_dir)
 
         self.assets_dir = assets_dir
-        self.disp = TFT.ST7735(
-            DC,
-            rst=RST,
-            spi=SPI.SpiDev(
-                SPI_PORT,
-                SPI_DEVICE,
-                max_speed_hz=SPEED_HZ))
 
-        # Initialize display.
-        self.disp.begin()
+        pygame.init()
+        rospy.loginfo("pygame init complete")
+        pygame.font.init()
+        pygame.mouse.set_cursor((8,8),(0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0))
+        pygame.mouse.set_visible(False)
+        self.disp = pygame.display.set_mode((HEIGHT, WIDTH))
+        rospy.loginfo("Display set mode complete")
 
         # Clear the display to a red background.
         # Can pass any tuple of red, green, blue values (from 0 to 255 each).
-        self.clear()
-        self.x_offset = 0
+        self.y_offset = 10
 
         # Load default font.
-        self.font = ImageFont.truetype(font, 16)
-
-        # Alternatively load a TTF font.
-        # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-        #font = ImageFont.truetype('Minecraftia.ttf', 16)
+        self.font = pygame.font.Font(font, 16)
+        rospy.loginfo("Font load complete")
 
         rospy.Subscriber("robot_commands", String, self.robot_state, queue_size=1)
 
-    # Define a function to create rotated text.  Unfortunately PIL doesn't have good
-    # native support for rotated fonts, but this function can be used to make a
-    # text image and rotate it so it's easy to paste in the buffer.
-    def draw_rotated_text(self, image, text, position, angle, font, fill=(255,255,255)):
-        # Get rendered font width and height.
-        draw = ImageDraw.Draw(image)
-        width, height = draw.textsize(text, font=font)
-        # Create a new image with transparent background to store the text.
-        textimage = Image.new('RGBA', (WIDTH, height))
-        # Render the text.
-        textdraw = ImageDraw.Draw(textimage)
-        textdraw.text((0,0), text, font=font, fill=fill)
-        # Rotate the text image.
-        rotated = textimage.rotate(angle, expand=1)
-        # Paste the text into the image, using it as a mask for transparency.
-        image.paste(rotated, position)
-        
-        self.disp.display()
+    def quit(self):
+        pygame.quit()
 
     def display_text(self, text):
-        self.draw_rotated_text(self.disp.buffer, text, (2, self.x_offset), 0, self.font, fill=(255,255,255))
-        self.x_offset = self.x_offset + 32
-        if self.x_offset > (HEIGHT - 32):
-            self.x_offset = 0
+        textsurface = self.font.render(text, False, (200, 0, 0))
+        
+        self.disp.blit(textsurface,(2, self.y_offset))
+        pygame.display.update()
+        
+        self.y_offset = self.y_offset + 28
+        if self.y_offset > (HEIGHT - 28):
+            self.y_offset = 10
 
     def display_image(self, filename):
         # Load an image.
-        image = Image.open("%s/%s" % (self.assets_dir, filename))
+        image = pygame.image.load("%s/%s" % (self.assets_dir, filename))
 
-        # Resize the image and rotate it so matches the display.
-        image = image.resize((HEIGHT, WIDTH))
-        self.disp.display(image)
+        self.disp.blit(image, [0,0])
+
+        pygame.display.update()
+
+    def play_movie(self, filename):
+        pygame.mixer.quit()
+        rospy.loginfo("Mixer quit")
+        movie = pygame.movie.Movie("%s/%s" % (self.assets_dir, filename))
+        rospy.loginfo("Movie loaded")
+        background = pygame.Surface((HEIGHT, WIDTH))
+        rospy.loginfo("Surface created")
+
+        self.disp.blit(background, (0, 0))
+        pygame.display.update()
+
+        mrect = pygame.Rect(0,0,128,100)
+        movie.set_display(self.disp, mrect.move(1, 1))
+        movie.play()
+
+        rospy.loginfo("Playing movie now")
+        while movie.get_busy() and movie.get_time() <= 6:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    break
+
+        rospy.loginfo("Finished playing movie")
+        movie.stop()
+        pygame.display.update()
 
     def robot_state(self, data):
         self.display_text(data.data)
 
     def clear(self):
-        self.disp.clear()
+        self.disp.fill((0,0,0))
 
 if __name__ == "__main__":
     rospy.init_node('display')
@@ -94,8 +98,10 @@ if __name__ == "__main__":
     assets_dir = rospy.get_param("assets")
     display = Display(font, assets_dir)
     
-    display.display_image("rising_sun.png")
     try:
+        display.play_movie("rising_sun.mpg")
+        time.sleep(7)
+        display.clear()
         rospy.spin()
     finally:
-        display.clear()
+        display.quit()
