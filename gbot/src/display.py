3f2853e071f0d682494e9bf35c9b032a7ca0a151
Gaurav Mathur <g@gmathur.com>
2018-05-24 05:54:48 +0000

    Just use mplayer to play movies

--- gbot/src/display.py
+++ gbot/src/display.py
@@ -2,11 +2,10 @@
 import rospy
 from std_msgs.msg import String
 from gbot.msg import RobotCmd
-import pygame
 import os
 import sys
 import time
-from pygame.locals import *
+import mplayer
 
 #os.environ['SDL_VIDEODRIVER'] = 'dummy' 
 os.environ["SDL_FBDEV"] = "/dev/fb1"
@@ -22,33 +21,33 @@ class Display:
         rospy.loginfo("Loading font %s assets dir %s", font, assets_dir)
 
         self.assets_dir = assets_dir
-
-        pygame.init()
-        rospy.loginfo("pygame init complete")
-        pygame.font.init()
-        pygame.mouse.set_cursor((8,8),(0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0))
-        pygame.mouse.set_visible(False)
-        self.disp = pygame.display.set_mode((HEIGHT, WIDTH))
-        rospy.loginfo("Display set mode complete")
+        self.player = mplayer.Player('-nolirc -vo fbdev2:/dev/fb1 -vf scale=128:-3')
+        #pygame.init()
+        #rospy.loginfo("pygame init complete")
+        #pygame.font.init()
+        #pygame.mouse.set_cursor((8,8),(0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0))
+        #pygame.mouse.set_visible(False)
+        #self.disp = pygame.display.set_mode((HEIGHT, WIDTH))
+        #rospy.loginfo("Display set mode complete")
 
         # Clear the display to a red background.
         # Can pass any tuple of red, green, blue values (from 0 to 255 each).
         self.y_offset = 10
 
         # Load default font.
-        self.font = pygame.font.Font(font, 16)
-        rospy.loginfo("Font load complete")
+        #self.font = pygame.font.Font(font, 16)
+        #rospy.loginfo("Font load complete")
 
         rospy.Subscriber("robot_commands", RobotCmd, self.robot_state, queue_size=1)
 
     def quit(self):
-        pygame.quit()
+        self.player.quit()
 
     def display_text(self, text):
-        textsurface = self.font.render(text, False, (200, 0, 0))
+        #textsurface = self.font.render(text, False, (200, 0, 0))
         
-        self.disp.blit(textsurface,(2, self.y_offset))
-        pygame.display.update()
+        #self.disp.blit(textsurface,(2, self.y_offset))
+        #pygame.display.update()
         
         self.y_offset = self.y_offset + 28
         if self.y_offset > (HEIGHT - 28):
@@ -56,42 +55,17 @@ class Display:
 
     def display_image(self, filename):
         # Load an image.
-        image = pygame.image.load("%s/%s" % (self.assets_dir, filename))
-
-        self.disp.blit(image, [0,0])
-
-        pygame.display.update()
+        pass
 
     def play_movie(self, filename):
-        pygame.mixer.quit()
-        rospy.loginfo("Mixer quit")
-        movie = pygame.movie.Movie("%s/%s" % (self.assets_dir, filename))
-        rospy.loginfo("Movie loaded")
-        background = pygame.Surface((HEIGHT, WIDTH))
-        rospy.loginfo("Surface created")
-
-        self.disp.blit(background, (0, 0))
-        pygame.display.update()
-
-        mrect = pygame.Rect(0,0,128,100)
-        movie.set_display(self.disp, mrect.move(1, 1))
-        movie.play()
-
-        rospy.loginfo("Playing movie now")
-        while movie.get_busy() and movie.get_time() <= 6:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    break
-
-        rospy.loginfo("Finished playing movie")
-        movie.stop()
-        pygame.display.update()
+        self.player.loadfile(os.path.join(self.assets_dir, 'rising_sun.mpg'))
 
     def robot_state(self, data):
         self.display_text(data.cmd)
 
     def clear(self):
-        self.disp.fill((0,0,0))
+        pass
+        #self.disp.fill((0,0,0))
 
 if __name__ == "__main__":
     rospy.init_node('display')
